ec00e337843443922e40748b27b1827b3fdaeb50
saifakib <mdsaifakib@gmail.com>
2023-02-19 20:35:06 +0600

    feat: User restaurant category currency and label

---
+++ api/src/api/label/controllers/label.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * label controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::label.label');
---
+++ api/src/api/reataurant/services/reataurant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reataurant service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::reataurant.reataurant');
