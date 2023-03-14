931da86c3d0c1f3791098b75106b84158d1ec67e
Azim Uddin Ahamed <azimuahmed13@gmail.com>
2023-03-14 21:04:46 +0600

    changes the software name

--- src/pages/DashboardAppPage copy.js
+++ src/pages/DashboardAppPage copy.js
@@ -26,7 +26,7 @@ export default function DashboardAppPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard | Minimal UI </title>
+        <title> Dashboard | Restaurant System </title>
       </Helmet>
 
       <Container maxWidth="xl">
